6574601a8c6724758e07ff4e5db3ebf5bb049dbf
Emma Buckley <ekbuckley@unomaha.edu>
2018-04-23 15:09:18 -0500

    New player sprite added, project heirarchy changed.

--- sound/SoundEffectManager.java
+++ sound/SoundEffectManager.java
@@ -3,10 +3,13 @@ package sound;
 public class SoundEffectManager extends SoundManager {
     public void playSound(String s) {
         switch (s) {
-            case "gunshot":
-                super.setupSound("res/soundclips/gunshot.wav", s);
-                oneShotStream.fire();
+            case "ghost1":
+                super.setupSound("soundclips/ghost1.wav", s);
+                oneShotClip.fire();
                 break;
+            case "ghost2":
+                super.setupSound("soundclips/ghost2.wav", s);
+                oneShotClip.fire();
         }
     }
 }
